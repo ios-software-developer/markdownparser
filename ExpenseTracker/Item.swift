81c01f87fd2c5ccf85db505f7dc6a23e0fda59ce
Anand S <anandios@me.com>
2026-03-06 14:44:38 +0200

    Initial Commit

---
+++ ExpenseTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ExpenseTracker
+//
+//  Created by SoftDev on 06.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
